eef23df94071608ce40f5426f80278b69a41f061
Jufarkas <139999007+Jufarkas@users.noreply.github.com>
2024-03-19 12:06:46 -0700

    rename 'toDo's to 'task', add comments

--- src/createCard.js
+++ src/createCard.js
@@ -1,32 +1,39 @@
+// creates template card that we hide using the 'wrapper' class; used for cloning in newNote.js
+
+
 import MarkCompleteIcon from './markComplete.svg';
 import DeleteIcon from './deleteTask.svg';
 import EditIcon from './editTask.svg';
 
 export function createCard() {
+    // create cardGrid to hold cards
     const cardGrid = document.querySelector('.card-grid');
+    // create div with 'wrapper' class, this is the only elem that receives this class
     const cardTemplateWrapper = document.createElement('div');
     cardTemplateWrapper.classList.add('wrapper');
-    const toDoCard = document.createElement('div');
-    toDoCard.classList.add('toDo-card');
+
+
+    const taskCard = document.createElement('div');
+    taskCard.classList.add('task-card');
 
     const task = document.createElement('button');
     task.classList.add('task');
 
     const taskTitleH3 = document.createElement('h3');
-    taskTitleH3.classList.add('toDo-title');
+    taskTitleH3.classList.add('task-title');
     
     const taskImportanceH6 = document.createElement('h6');
-    taskImportanceH6.classList.add('toDo-priority');
+    taskImportanceH6.classList.add('task-priority');
      
     const taskDueDateH6 = document.createElement('h6');
-    taskDueDateH6.classList.add('toDo-date');
+    taskDueDateH6.classList.add('task-date');
     
     const taskText = document.createElement('p');
-    taskText.classList.add('toDo-para');
+    taskText.classList.add('task-para');
 
     cardGrid.appendChild(cardTemplateWrapper);
-    cardTemplateWrapper.appendChild(toDoCard);
-    toDoCard.appendChild(task);
+    cardTemplateWrapper.appendChild(taskCard);
+    taskCard.appendChild(task);
     task.appendChild(taskTitleH3);
     task.appendChild(taskImportanceH6);
     task.appendChild(taskDueDateH6);
@@ -34,7 +41,7 @@ export function createCard() {
 
     const cardButtons = document.createElement('div');
     cardButtons.classList.add('card-buttons');
-    toDoCard.appendChild(cardButtons);
+    taskCard.appendChild(cardButtons);
 
     const completeBtn = document.createElement('button');
     completeBtn.textContent = "Complete";
