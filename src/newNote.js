9525528d22bd25b5ff2320ebb29bf8268b35cb1a
Jufarkas <139999007+Jufarkas@users.noreply.github.com>
2024-03-21 17:18:29 -0700

    rewrite fnc to edit changes to note; to reflect new edit dialog

--- src/newNote.js
+++ src/newNote.js
@@ -11,19 +11,17 @@ export class NewNote {
         this.importance = importance;
         this.dueDate = dueDate;
         this.description = description;
-    }
+    };
 
     static createNewNote(title, importance, dueDate, description){
         if(this.myNotes.hasOwnProperty(title)){
-            alert("Sorry no duplicates allowed");
+            alert("This note title already exists!");
             return;
         }
-        //grab dialog; to close once finished creating new note
-        const dialog = document.querySelector('dialog');
-        //wrapper contains "hidden" card template that we clone in the .forEach below
-        const wrapper = document.querySelector('.wrapper');
-        //cardGrid needed to append new cards to
-        const cardGrid = document.querySelector('.card-grid');
+
+        const newTaskDialog = document.querySelector('.newTaskDialog'); // grab dialog to close when finished
+        const wrapper = document.querySelector('.wrapper'); // contains "hidden" card we clone
+        const cardGrid = document.querySelector('.card-grid'); // cardGrid to append new cards
 
         let task = document.querySelector('.task');
         let newTitle = document.querySelector('.task-title');
@@ -35,23 +33,66 @@ export class NewNote {
         newDueDate.textContent = 'Due: ' + dueDate;
         newDescription.textContent = description;
 
-        //have to remove the class as it continuously adds it regardless if a note is marked "normal"; then check importance again and add to 'important' only note(s)
-        task.classList.remove('importantNote');
+        task.classList.remove('importantNote'); //need this otherwise it continuously adds the class regardless of selection
         
-        //checks if "important" is selected and applies relevant class
         if (importance === "Important"){
-            task.classList.add('importantNote');
-        }
+            task.classList.add('importantNote'); //checks if "important" is selected and applies relevant class
+        };
 
         this.myNotes[title] = new NewNote(title, importance, dueDate, description);
 
-        // clones all children within our hidden 'wrapper' card. This way, it has all the appropriate elems for every new note added
+        // clones children in hidden wrapper to create new notes
         let children = wrapper.childNodes;
         children.forEach((item) => {
             let newNote = item.cloneNode(true);
             cardGrid.appendChild(newNote);
         });
-        console.log(this.myNotes); // *************** remove once finished
-        dialog.close();
+        console.log(this.myNotes); // ************************************ remove after complete
+        newTaskDialog.close();
+    };
+
+    static editNote(oldTitle, title, importance, dueDate, description){
+        if(this.myNotes.hasOwnProperty(title) && oldTitle !== title){
+            alert("This note title already exists!");
+            return;
+        }
+
+        const editTaskDialog = document.querySelector('.editTaskDialog');
+        const target = document.querySelector('.' + oldTitle);
+        // grab existing note from the original title we stored in oldTitle
+        const note = this.myNotes[oldTitle];
+        // delete it
+        delete this.myNotes[oldTitle];
+
+        // add new note with new title
+        this.myNotes[title] = note;
+        this.myNotes[title].title = title;
+        this.myNotes[title].importance = importance;
+        this.myNotes[title].dueDate = dueDate;
+        this.myNotes[title].description = description;
+
+
+        let newTitle = target.querySelector('.task-title');
+        let newImportance = target.querySelector('.task-priority');
+        let newDueDate = target.querySelector('.task-date');
+        let newDescription = target.querySelector('.task-para');
+        newTitle.textContent = title;
+        newImportance.textContent = "Importance: " + importance;
+        newDueDate.textContent = 'Due: ' + dueDate;
+        newDescription.textContent = description;
+
+        importanceChangeCheck(importance, target);
+        console.log(this.myNotes); // ************************************ remove after complete
+        target.classList.remove(oldTitle);
+        target.classList.add(title);
+        editTaskDialog.close();
+    };
+};
+
+function importanceChangeCheck(importance, target) {
+    if (importance === "Normal") {
+        target.classList.remove('importantNote');
+    } else {
+        target.classList.add('importantNote');
     }
 }
